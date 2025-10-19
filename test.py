9fdc77d4cb7365c0c2dc855605fd5b08f0b4976c
Neo Östlund Zetterberg <neo.ostlundzetterbe@learnet.se>
2025-10-19 14:09:12 +0200

    Fixed test.

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from Terminal import *
 import time
 
 progress = ProgressBar("$gre$bri[has]$yel$dim[need]$res ($gre[prog]%$res)", "-", 10)
-for i in range(10):
+for i in range(11):
     index = progress.calc_index(i, 10)
     progress.print_frame("\r", index=index, end="")
     time.sleep(1)
